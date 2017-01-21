1e7f69c94e63f719f2f70229278ff1310557d4ac
darthendorman <agao.oa@gmail.com>
2017-01-20 23:46:41 -0500

    Bullet
    It works!!!!

--- Assets/Scripts/WandController.cs
+++ Assets/Scripts/WandController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class WandController : SteamVR_TrackedController {
 
+	//Use the bullet prefab from the Component Inspector
+	public GameObject Bullet; 
+
+	//Use the speed of the bullet from the Component Insepctor
+	public float Bullet_Force;
+
 	// Use this for initialization
 	protected override void Start () {
 		base.Start ();
@@ -17,6 +23,20 @@ public class WandController : SteamVR_TrackedController {
 	public override void OnTriggerClicked(ClickedEventArgs e)
 	{
 		base.OnTriggerClicked (e);
+
+		GameObject Temporary_Bullet;
+		Temporary_Bullet = Instantiate (Bullet, this.transform.position, this.transform.rotation) as GameObject;
+
+		Rigidbody Temporary_Rigidbody;
+		Temporary_Rigidbody = Temporary_Bullet.GetComponent<Rigidbody> ();
+
+		Temporary_Rigidbody.AddForce(transform.forward * Bullet_Force);
+
+		// Self destruct after 10 seconds
+		Destroy(Temporary_Bullet, 10.0f);
+
 	}
+
+
 
 }
