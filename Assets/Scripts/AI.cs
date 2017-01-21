8d625e662f4e02c070c9f1de8408b11ba150f921
darthendorman <agao.oa@gmail.com>
2017-01-21 16:29:51 -0500

    Enemy Spawn + Movement
    Is working

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,37 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour {
 
-	public float enemyMovementSpeed;
-	public float damping;
-	public Transform target;
-	Rigidbody rigidbody;
-	Renderer myRender;
+	public Transform destination;
 
-	// Use this for initialization
-	void Start () {
-		myRender.GetComponent<Renderer> ();	
-		rigidbody = GetComponent<Rigidbody> ();
+	private NavMeshAgent agent;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		LookAtPlayer ();
-		AttackPlayer ();
-	}
+	public GameObject target;
 
-	void LookAtPlayer()
+	void Start()
 	{
-		Quaternion rotation = Quaternion.LookRotation (target.position - transform.position);
-		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
-	}
-
-	void AttackPlayer()
-	{
-		rigidbody.AddForce (transform.forward * enemyMovementSpeed);
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		destination = target.transform;
+		agent.SetDestination(destination.position);
 	}
 
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,17 +6,18 @@ public class GameController : MonoBehaviour {
 
 	public GameObject[] objects;
 	public GameObject enemy;                // The enemy prefab to be spawned.
-	public float spawnTime = 3f;            // How long between each spawn.
-	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	public float spawnTime = 3f;            // How long between each spawn
+	public Transform[] spawnPoints;
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		InvokeRepeating ("SpawnRandom", spawnTime, spawnTime);
 	}
 
 	public void SpawnRandom()
 	{
-		Instantiate(enemy);
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 	}
 	
 	
