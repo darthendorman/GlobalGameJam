ccf78a17a1854369a43abf0b53be02a55f357d02
darthendorman <agao.oa@gmail.com>
2017-01-21 16:10:06 -0500

    AI + Spawn
    Added random spawning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+	public GameObject[] objects;
+	public GameObject enemy;                // The enemy prefab to be spawned.
+	public float spawnTime = 3f;            // How long between each spawn.
+	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
 	// Use this for initialization
 	void Start () {
-		
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
 	}
+
+	public void SpawnRandom()
+	{
+		Instantiate(enemy);
+	}
+	
 	
 	// Update is called once per frame
 	void Update () {
