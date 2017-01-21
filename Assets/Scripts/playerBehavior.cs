bb2bd29b027fedf86fea40c38b7dd613848bc41b
Vendeta25 <rhavanas@gmail.com>
2017-01-21 18:30:51 -0500

    Health partway done needs collison

---
+++ Assets/Scripts/playerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerBehavior : MonoBehaviour {
+    public float health;
+    public GameObject healthBar;
+    float totalHealth;
+	// Use this for initialization
+	void Start () {
+        health = 100;
+        totalHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateHealth(healthBar);
+	}
+
+    void updateHealth(GameObject healthBar)
+    {
+        healthBar.transform.localScale = new Vector3(health/totalHealth, transform.localScale.y,transform.localScale.z);
+    }
+    void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
